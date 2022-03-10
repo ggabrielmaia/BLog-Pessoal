e651050a8023cf6735794ea348b29b3e24358949
unknown <gabriel,maia1333@gmail.com>
2022-03-10 14:32:59 -0300

    Adicionando toastfy

--- src/components/postagens/tabpostagem/TabPostagem.tsx
+++ src/components/postagens/tabpostagem/TabPostagem.tsx
@@ -26,7 +26,9 @@ function TabPostagem() {
         </TabPanel>
         <TabPanel value="2">
           <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo">Sobre-nós</Typography>
-          <Typography variant="body1" gutterBottom color="textPrimary" align="justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ut eveniet natus totam et, voluptate dicta tempore alias, odio nobis non eius cupiditate minima inventore pariatur! Ipsum itaque consectetur voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo velit consequuntur suscipit fugiat, nam quis quod quaerat veritatis et, vel ratione beatae, facere neque! Quo animi porro voluptate saepe deleniti? Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore adipisci, officia aut quidem dolorum deserunt iure dolorem doloribus velit nobis quas consequatur at ullam odit, nesciunt est nulla nihil excepturi!</Typography>
+          <Typography variant="body1" gutterBottom color="textPrimary" align="justify">Olá! Meu nome é gabriel maia, tenho 18 anos e 
+          esse é meu blog pessoal! Fique a vontade para postar assuntos relacionados a tecnologia, criar
+          novos topicos e novas postagens.</Typography>
         </TabPanel>
       </TabContext>
     </>
